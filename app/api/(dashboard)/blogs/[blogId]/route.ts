08db36a7e0a8ae1bd9d11534ba5cfd7aba8d8d04
Joshua Gonzales <codingjogo@gmail.com>
2024-12-09 16:14:12 +0800

    feat: Update a Blog Post

--- app/api/(dashboard)/blogs/[blogId]/route.ts
+++ app/api/(dashboard)/blogs/[blogId]/route.ts
@@ -73,8 +73,99 @@ export const GET = async (req: Request, {params} : {
 		);
 	} catch (error: // eslint-disable-next-line
 	any) {
-		return new NextResponse("Error in fetching blogs" + error.message, {
+		return new NextResponse("Error in fetching blog" + error.message, {
 			status: 500,
 		});
 	}
 };
+
+export const PATCH = async (req: Request, {params} : {
+  params: {
+    blogId: string;
+  }
+}) => {
+	try {
+		const { searchParams } = new URL(req.url);
+		const userId = searchParams.get("userId");
+		const categoryId = searchParams.get("categoryId");
+    const blogId = params.blogId;
+		const {title, description} = await req.json();
+
+		if (!userId || !Types.ObjectId.isValid(userId)) {
+			return new NextResponse("No User ID found!", {
+				status: 400,
+			});
+		}
+
+		if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+			return new NextResponse("No Category ID found!", {
+				status: 400,
+			});
+		}
+
+    if (!blogId || !Types.ObjectId.isValid(blogId)) {
+			return new NextResponse("No Blog ID found!", {
+				status: 400,
+			});
+		}
+
+		if (!title || !description) {
+			return new NextResponse("Please input required fields!", {
+				status: 400,
+			});
+		}
+
+		await connect();
+
+		const user = await User.findById(userId);
+		if (!user) {
+			return new NextResponse("User not found in the DB!", {
+				status: 400,
+			});
+		}
+
+		const category = await Category.findById(categoryId);
+		if (!category) {
+			return new NextResponse("Category not found in the DB!", {
+				status: 400,
+			});
+		}
+
+		const blog = await Blog.findOne(
+      {
+        _id: blogId,
+        user: userId,
+        category: categoryId
+      }
+    );
+    if (!blog) {
+			return new NextResponse("Blog not found in the DB!", {
+				status: 400,
+			});
+		}
+
+		const updatedBlog = await Blog.findByIdAndUpdate(
+			blogId,
+			{ title, description },
+			{ new: true }
+		)
+
+		if (!updatedBlog) {
+			return new NextResponse("Something went wrong, please try again to update the blog", {
+				status: 400,
+			});
+		}
+
+		return new NextResponse(JSON.stringify({
+			message: 'Successful Updating the Blog!',
+			updatedBlog
+		}), {
+			status: 200,
+		});
+	} catch (error: // eslint-disable-next-line
+		any) {
+			return new NextResponse("Error in fetching blogs" + error.message, {
+				status: 500,
+			});
+		}
+}
